26164ca85d640c6ee4471022359b1fdcd0329cfc
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-12 05:22:52 -0700

    add bottombar widget

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:bestlocaleats/widgets/bottom_bar.dart';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -96,33 +98,39 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Stack(
-        children: [
-          Container(
-            // image below the top bar
-            child: SizedBox(
-              height: screenSize.height * 0.45,
-              width: screenSize.width,
-              child: Image.asset(
-                'assets/images/cover.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Center(
-            heightFactor: 1,
-            child: Padding(
-              padding: EdgeInsets.only(
-                top: screenSize.height * 0.40,
-                left: screenSize.width / 5,
-                right: screenSize.width / 5,
-              ),
-              child: Card(// floating quick access bar
-                  // ...
+      body: SingleChildScrollView(
+        physics: ClampingScrollPhysics(),
+        child: Column(children: [
+          Stack(
+            children: [
+              Container(
+                // image below the top bar
+                child: SizedBox(
+                  height: screenSize.height * 0.45,
+                  width: screenSize.width,
+                  child: Image.asset(
+                    'assets/images/cover.png',
+                    fit: BoxFit.cover,
                   ),
-            ),
-          )
-        ],
+                ),
+              ),
+              Center(
+                heightFactor: 1,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: screenSize.height * 0.40,
+                    left: screenSize.width / 5,
+                    right: screenSize.width / 5,
+                  ),
+                  child: Card(// floating quick access bar
+                      // ...
+                      ),
+                ),
+              )
+            ],
+          ),
+          BottomBar(),
+        ]),
       ),
     );
   }
