bb6542a93481cbf1643d3d33f0e08fb22dbd1c1b
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-14 11:01:28 -0700

    login UI update

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -500,23 +500,362 @@ class _LoginPageState extends State<LoginPage> {
           )),
       body: SingleChildScrollView(
         physics: const ClampingScrollPhysics(),
-        child: Column(children: [
-          Stack(
-            children: [
-              Container(
-                // image below the top bar
-                child: SizedBox(
-                  height: screenSize.height * 0.45,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: Constants.mainPadding, vertical: 20),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
                   width: screenSize.width,
-                  child: Image.asset(
-                    'assets/images/cover.png',
-                    fit: BoxFit.cover,
-                  ),
+                  child: Center(
+                    child: Image.asset(Constants.IMG_START,
+                        width: screenSize.width * 0.5,
+                        height: screenSize.width * 0.5),
+                  )),
+              const SizedBox(height: 20),
+              const Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: Constants.mainPadding),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                      color: CustomColor.primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 38),
                 ),
+              ),
+              const SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: Constants.mainPadding * 2),
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: 52,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(4)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: CustomColor.primaryColor
+                                        .withOpacity(0.2),
+                                    blurRadius: 5,
+                                    spreadRadius: 1,
+                                    offset: const Offset(0, 0)),
+                              ],
+                            ),
+                          ),
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter your email',
+                            ),
+                            validator: (value) {
+                              return _validateEmail(value!);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(
+                          Constants.mainPadding * 2,
+                          8,
+                          Constants.mainPadding * 2,
+                          0),
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: 52,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(4)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: CustomColor.primaryColor
+                                        .withOpacity(0.2),
+                                    blurRadius: 5,
+                                    spreadRadius: 1,
+                                    offset: const Offset(0, 0)),
+                              ],
+                            ),
+                          ),
+                          TextFormField(
+                            controller: _passController,
+                            obscureText: _obscureText,
+                            decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                hintText: 'Password',
+                                suffixIconConstraints: const BoxConstraints(
+                                  minWidth: 50,
+                                  minHeight: 2,
+                                ),
+                                suffixIcon: InkWell(
+                                    onTap: _toggle,
+                                    child: Icon(
+                                        _obscureText
+                                            ? Icons.remove_red_eye_outlined
+                                            : Icons.visibility_off_outlined,
+                                        color: CustomColor.textSecondaryColor,
+                                        size: 24))),
+                            validator: (value) {
+                              return _validatePassword(value!);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: Constants.mainPadding * 2,
+                              vertical: 2),
+                          child: InkWell(
+                            onHover: (value) {
+                              setState(() {
+                                value
+                                    ? _isHovering[1] = true
+                                    : _isHovering[1] = false;
+                              });
+                            },
+                            onTap: () {},
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                color: _isHovering[1]
+                                    ? CustomColor.activeColor
+                                    : CustomColor.textSecondaryColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: Constants.mainPadding * 2),
+                      child: SizedBox(
+                          height: 40,
+                          width: screenSize.width,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: CustomColor.primaryColor,
+                                elevation: 3,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(4)),
+                                shadowColor:
+                                    CustomColor.primaryColor.withOpacity(0.5),
+                                padding: const EdgeInsets.all(5)),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text('Processing Data')),
+                                );
+                              }
+                            },
+                            child: const Text(
+                              'Continue',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16.0,
+                                  letterSpacing: 1),
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10, horizontal: Constants.mainPadding),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Container(
+                              width: screenSize.width * 0.2,
+                              height: 1,
+                              color: CustomColor.textPrimaryColor),
+                        ),
+                        const Text('OR', style: TextStyle(fontSize: 12)),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Container(
+                              width: screenSize.width * 0.2,
+                              height: 1,
+                              color: CustomColor.textPrimaryColor),
+                        ),
+                      ])),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10, horizontal: Constants.mainPadding * 2),
+                child: SizedBox(
+                    height: 40,
+                    width: screenSize.width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4)),
+                          shadowColor:
+                              CustomColor.primaryColor.withOpacity(0.5),
+                          padding: const EdgeInsets.all(5)),
+                      onPressed: () {},
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(Constants.IMG_GOOGLE1),
+                            const SizedBox(width: 5),
+                            const Text(
+                              'Continue With Google',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16.0),
+                            ),
+                          ]),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10, horizontal: Constants.mainPadding * 2),
+                child: SizedBox(
+                    height: 40,
+                    width: screenSize.width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4)),
+                          shadowColor:
+                              CustomColor.primaryColor.withOpacity(0.5),
+                          padding: const EdgeInsets.all(5)),
+                      onPressed: () {},
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(Constants.IMG_FACEBOOK1),
+                            const SizedBox(width: 5),
+                            Text(
+                              screenSize.width < 900
+                                  ? 'Continue With Face...'
+                                  : 'Continue With Facebook',
+                              style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16.0,
+                                  overflow: TextOverflow.fade),
+                            ),
+                          ]),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10, horizontal: Constants.mainPadding * 2),
+                child: SizedBox(
+                    height: 40,
+                    width: screenSize.width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 3,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4)),
+                          shadowColor:
+                              CustomColor.primaryColor.withOpacity(0.5),
+                          padding: const EdgeInsets.all(5)),
+                      onPressed: () {},
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(Constants.IMG_APPLE1),
+                            const SizedBox(width: 12),
+                            const Text(
+                              'Continue With Apple',
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16.0),
+                            ),
+                          ]),
+                    )),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15, horizontal: Constants.mainPadding),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Container(
+                              width: screenSize.width * 0.2,
+                              height: 1,
+                              color: CustomColor.textPrimaryColor),
+                        ),
+                        const Text('OR', style: TextStyle(fontSize: 12)),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Container(
+                              width: screenSize.width * 0.2,
+                              height: 1,
+                              color: CustomColor.textPrimaryColor),
+                        ),
+                      ])),
+              const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Log in with Best Local Eats app',
+                    style: TextStyle(fontSize: 15),
+                  )),
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.center,
+                child: Row(children: [
+                  Checkbox(
+                    checkColor: Colors.white,
+                    fillColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.disabled)) {
+                        return Colors.black;
+                      }
+                      return Colors.black;
+                    }),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(3)),
+                    value: _isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _isChecked = value!;
+                      });
+                    },
+                  ),
+                  Expanded(
+                      child: RichText(
+                    text: TextSpan(children: [
+                      const TextSpan(
+                        text: 'By Signing up i Agree to the',
+                      ),
+                      const TextSpan(text: ' '),
+                      TextSpan(
+                          text: 'term and Condition & privacy Police',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                          recognizer: TapGestureRecognizer()..onTap = () {}),
+                    ]),
+                  )),
+                ]),
               ),
             ],
           ),
-        ]),
+        ),
       ),
     );
   }
