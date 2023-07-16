a52a6ff851ffd988c215e5794938eb6d51455b17
mercurystudio99 <mercurystudio99@gmail.com>
2023-07-16 05:44:40 -0700

    add bottom bar

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,71 +1,293 @@
+import 'package:bestlocaleats/widgets/responsive.dart';
 import 'package:bestlocaleats/widgets/bottom_bar_column.dart';
-import 'package:bestlocaleats/widgets/info_text.dart';
+import 'package:bestlocaleats/utils/constants.dart';
+import 'package:bestlocaleats/utils/colors.dart';
+import 'package:bestlocaleats/utils/router.dart';
 import 'package:flutter/material.dart';
 
-class BottomBar extends StatelessWidget {
+class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
 
   @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
   Widget build(BuildContext context) {
+    if (ResponsiveWidget.isSmallScreen(context)) {
+      return _mobile();
+    } else {
+      return _desktop();
+    }
+  }
+
+  Widget _desktop() {
+    Size screenSize = MediaQuery.of(context).size;
     return Container(
-      padding: EdgeInsets.all(30),
-      color: Colors.blueGrey[900],
+      padding: const EdgeInsets.symmetric(
+          horizontal: Constants.mainPadding, vertical: 70),
+      color: CustomColor.primaryColor,
       child: Column(
         children: [
           Row(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              BottomBarColumn(
-                heading: 'ABOUT',
-                s1: 'Contact Us',
-                s2: 'About Us',
-                s3: 'Careers',
-              ),
-              BottomBarColumn(
-                heading: 'HELP',
-                s1: 'Payment',
-                s2: 'Cancellation',
-                s3: 'FAQ',
-              ),
-              BottomBarColumn(
-                heading: 'SOCIAL',
-                s1: 'Twitter',
-                s2: 'Facebook',
-                s3: 'YouTube',
-              ),
-              Container(
-                color: Colors.blueGrey,
-                width: 2,
-                height: 150,
-              ),
-              Column(
+              Expanded(
+                  child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  InfoText(
-                    type: 'Email',
-                    text: 'explore@gmail.com',
+                  InkWell(
+                    onHover: (value) {},
+                    onTap: () {
+                      NavigationRouter.switchToHomePage(context);
+                    },
+                    child: Image.asset(
+                      Constants.IMG_LOGO,
+                    ),
                   ),
-                  SizedBox(height: 5),
-                  InfoText(
-                    type: 'Address',
-                    text: '128, Trymore Road, Delft, MN - 56124',
-                  )
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: RichText(
+                      text: const TextSpan(children: [
+                        TextSpan(
+                            text:
+                                'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                            style: TextStyle(color: Colors.white)),
+                      ]),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Image.asset(Constants.IMG_FACEBOOK, width: 40),
+                    const SizedBox(width: 30),
+                    Image.asset(Constants.IMG_APPLE, width: 40),
+                    const SizedBox(width: 30),
+                    Image.asset(Constants.IMG_GOOGLE, width: 40),
+                    const SizedBox(width: 30),
+                  ]),
                 ],
-              ),
+              )),
+              const Expanded(
+                  child: BottomBarColumn(
+                'Our Menu',
+                'Home',
+                'All restaurant',
+                'Contact us',
+                'Favorites',
+              )),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Newsletter',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: RichText(
+                      text: const TextSpan(children: [
+                        TextSpan(
+                            text:
+                                'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                            style: TextStyle(color: Colors.white)),
+                      ]),
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: SizedBox(
+                        width: screenSize.width * 0.25,
+                        child: TextField(
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.email_outlined,
+                                color: Colors.white),
+                            suffixIcon: Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: InkWell(
+                                    onHover: (value) {},
+                                    onTap: () {},
+                                    child: Image.asset(
+                                      Constants.IMG_ARROW,
+                                      width: 60,
+                                    ))),
+                            enabledBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)),
+                            focusedBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)),
+                            filled: true,
+                            fillColor: Colors.transparent,
+                            hintText: 'Your Mail Address',
+                            hintStyle: const TextStyle(
+                                color: CustomColor.textSecondaryColor),
+                          ),
+                        ),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: RichText(
+                      text: const TextSpan(children: [
+                        TextSpan(
+                            text: 'T: 0123456789',
+                            style: TextStyle(color: Colors.white)),
+                      ]),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: RichText(
+                      text: const TextSpan(children: [
+                        TextSpan(
+                            text: 'E: bestlocaleats@gmail.com',
+                            style: TextStyle(color: Colors.white)),
+                      ]),
+                    ),
+                  ),
+                ],
+              ))
             ],
           ),
-          Divider(
-            color: Colors.blueGrey,
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Copyright © 2020 | EXPLORE',
-            style: TextStyle(
-              color: Colors.blueGrey[300],
-              fontSize: 14,
-            ),
-          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _mobile() {
+    Size screenSize = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          horizontal: Constants.mainPadding, vertical: 70),
+      color: CustomColor.primaryColor,
+      child: Column(
+        children: [
+          SizedBox(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              InkWell(
+                onHover: (value) {},
+                onTap: () {
+                  NavigationRouter.switchToHomePage(context);
+                },
+                child: Image.asset(
+                  Constants.IMG_LOGO,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: RichText(
+                  text: const TextSpan(children: [
+                    TextSpan(
+                        text:
+                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                        style: TextStyle(color: Colors.white)),
+                  ]),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Image.asset(Constants.IMG_FACEBOOK, width: 40),
+                const SizedBox(width: 30),
+                Image.asset(Constants.IMG_APPLE, width: 40),
+                const SizedBox(width: 30),
+                Image.asset(Constants.IMG_GOOGLE, width: 40),
+                const SizedBox(width: 30),
+              ]),
+            ],
+          )),
+          const SizedBox(height: 50),
+          const SizedBox(
+              child: BottomBarColumn(
+            'Our Menu',
+            'Home',
+            'All restaurant',
+            'Contact us',
+            'Favorites',
+          )),
+          const SizedBox(height: 50),
+          SizedBox(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Newsletter',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: RichText(
+                  text: const TextSpan(children: [
+                    TextSpan(
+                        text:
+                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                        style: TextStyle(color: Colors.white)),
+                  ]),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: SizedBox(
+                    width: screenSize.width * 0.7,
+                    child: TextField(
+                      style: const TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.email_outlined,
+                            color: Colors.white),
+                        suffixIcon: Padding(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: InkWell(
+                                onHover: (value) {},
+                                onTap: () {},
+                                child: Image.asset(
+                                  Constants.IMG_ARROW,
+                                  width: 60,
+                                ))),
+                        enabledBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: const UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        hintText: 'Your Mail Address',
+                        hintStyle: const TextStyle(
+                            color: CustomColor.textSecondaryColor),
+                      ),
+                    ),
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: RichText(
+                  text: const TextSpan(children: [
+                    TextSpan(
+                        text: 'T: 0123456789',
+                        style: TextStyle(color: Colors.white)),
+                  ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: RichText(
+                  text: const TextSpan(children: [
+                    TextSpan(
+                        text: 'E: bestlocaleats@gmail.com',
+                        style: TextStyle(color: Colors.white)),
+                  ]),
+                ),
+              ),
+            ],
+          ))
         ],
       ),
     );
--- lib/widgets/bottom_bar_column.dart
+++ lib/widgets/bottom_bar_column.dart
@@ -1,62 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:bestlocaleats/utils/colors.dart';
 
-class BottomBarColumn extends StatelessWidget {
+class BottomBarColumn extends StatefulWidget {
   final String heading;
   final String s1;
   final String s2;
   final String s3;
+  final String s4;
 
-  const BottomBarColumn({
-    super.key,
-    required this.heading,
-    required this.s1,
-    required this.s2,
-    required this.s3,
-  });
+  const BottomBarColumn(this.heading, this.s1, this.s2, this.s3, this.s4,
+      {super.key});
+
+  @override
+  State<BottomBarColumn> createState() => _BottomBarColumnState();
+}
+
+class _BottomBarColumnState extends State<BottomBarColumn> {
+  final List _isHovering = [
+    false, // 0: home
+    false, // 1: all restaurant
+    false, // 2: contact us
+    false, // 3: favorite
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            heading,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.heading,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 14,
+        ),
+        InkWell(
+          onHover: (value) {
+            setState(() {
+              value ? _isHovering[0] = true : _isHovering[0] = false;
+            });
+          },
+          onTap: () {},
+          child: Text(
+            widget.s1,
             style: TextStyle(
-              color: Colors.blueGrey[300],
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-            ),
+                color: _isHovering[0] ? CustomColor.activeColor : Colors.white,
+                fontSize: 14,
+                letterSpacing: 1),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            s1,
+        ),
+        const SizedBox(height: 8),
+        InkWell(
+          onHover: (value) {
+            setState(() {
+              value ? _isHovering[1] = true : _isHovering[1] = false;
+            });
+          },
+          onTap: () {},
+          child: Text(
+            widget.s2,
             style: TextStyle(
-              color: Colors.blueGrey[100],
-              fontSize: 14,
-            ),
+                color: _isHovering[1] ? CustomColor.activeColor : Colors.white,
+                fontSize: 14,
+                letterSpacing: 1),
           ),
-          SizedBox(height: 5),
-          Text(
-            s2,
+        ),
+        const SizedBox(height: 8),
+        InkWell(
+          onHover: (value) {
+            setState(() {
+              value ? _isHovering[2] = true : _isHovering[2] = false;
+            });
+          },
+          onTap: () {},
+          child: Text(
+            widget.s3,
             style: TextStyle(
-              color: Colors.blueGrey[100],
-              fontSize: 14,
-            ),
+                color: _isHovering[2] ? CustomColor.activeColor : Colors.white,
+                fontSize: 14,
+                letterSpacing: 1),
           ),
-          SizedBox(height: 5),
-          Text(
-            s3,
+        ),
+        const SizedBox(height: 8),
+        InkWell(
+          onHover: (value) {
+            setState(() {
+              value ? _isHovering[3] = true : _isHovering[3] = false;
+            });
+          },
+          onTap: () {},
+          child: Text(
+            widget.s4,
             style: TextStyle(
-              color: Colors.blueGrey[100],
-              fontSize: 14,
-            ),
+                color: _isHovering[3] ? CustomColor.activeColor : Colors.white,
+                fontSize: 14,
+                letterSpacing: 1),
           ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 8),
+      ],
     );
   }
 }
